881388b718b09664b5ccbb5e4dcd34dcfd2b980f
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2024-03-14 20:59:29 -0300

    Add files via upload

--- Atividade 2/index.php
+++ Atividade 2/index.php
@@ -6,34 +6,34 @@
     <title>Verificador de Par ou Ímpar</title>
 </head>
 <body>
-    <div id="tformulario">
-        <h2>Verificador de Par ou Ímpar</h2>
-        <form method="post">
-            <label for="numero">Insira um número inteiro:</label>
-            <input type="text" id="numero" name="numero">
-            <button type="submit">Verificar</button>
-    
-            <?php
-            // Verifica se o formulário foi submetido
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                // Obtém o número digitado pelo usuário
-                $numero = $_POST["numero"];
+    <div class="container">
+        <div class="form-container">
+            <h2>Verificador de Par ou Ímpar</h2>
+            <form method="post">
+                <label for="numero">Insira um número inteiro:</label>
+                <input type="text" id="numero" name="numero">
+                <button type="submit">Verificar</button>
+                <?php
+                // Verifica se o formulário foi submetido
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    // Obtém o número digitado pelo usuário
+                    $numero = $_POST["numero"];
 
-                // Verifica se o número é um inteiro
-                if (filter_var($numero, FILTER_VALIDATE_INT) !== false) {
-                    // Verifica se o número é par ou ímpar
-                    if ($numero % 2 == 0) {
-                        echo "<p>O número $numero é par.</p>";
+                    // Verifica se o número é um inteiro
+                    if (filter_var($numero, FILTER_VALIDATE_INT) !== false) {
+                        // Verifica se o número é par ou ímpar
+                        if ($numero % 2 == 0) {
+                            echo "<p>O número $numero é par.</p>";
+                        } else {
+                            echo "<p>O número $numero é ímpar.</p>";
+                        }
                     } else {
-                        echo "<p>O número $numero é ímpar.</p>";
+                        echo "<p>Por favor, insira um número inteiro válido.</p>";
                     }
-                } else {
-                    echo "<p>Por favor, insira um número inteiro válido.</p>";
                 }
-            }
-            ?>
-        </form>
+                ?>
+            </form>
+        </div>
     </div>
-    
 </body>
 </html>
