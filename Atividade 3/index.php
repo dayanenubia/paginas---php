e41581fa886660fe615b8b738bab051caa12937c
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2024-03-14 21:12:05 -0300

    Add files via upload

--- Atividade 3/index.php
+++ Atividade 3/index.php
@@ -2,40 +2,43 @@
 <html>
 <head>
     <title>Identificar o maior número</title>
+    <link rel="stylesheet" type="text/css" href="css/form.css">
 </head>
 <body>
-    <h2>Identificar o maior número</h2>
-    <form method="post">
-        <label for="num1">Número 1:</label>
-        <input type="number" name="num1" id="num1" required><br><br>
-        
-        <label for="num2">Número 2:</label>
-        <input type="number" name="num2" id="num2" required><br><br>
-        
-        <label for="num3">Número 3:</label>
-        <input type="number" name="num3" id="num3" required><br><br>
-        
-        <input type="submit" name="submit" value="Identificar o maior número">
-    </form>
+    <div class="container">
+        <h2>Identificar o maior número</h2>
+        <form method="post">
+            <label for="num1">Número 1:</label>
+            <input type="number" name="num1" id="num1" required><br><br>
+            
+            <label for="num2">Número 2:</label>
+            <input type="number" name="num2" id="num2" required><br><br>
+            
+            <label for="num3">Número 3:</label>
+            <input type="number" name="num3" id="num3" required><br><br>
+            
+            <input type="submit" name="submit" value="Identificar o maior número">
+        </form>
 
-    <?php
-    if(isset($_POST['submit'])){
-        $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
-        $num3 = $_POST['num3'];
+        <?php
+        if(isset($_POST['submit'])){
+            $num1 = $_POST['num1'];
+            $num2 = $_POST['num2'];
+            $num3 = $_POST['num3'];
 
-        $maior_numero = $num1;
+            $maior_numero = $num1;
 
-        if ($num2 > $maior_numero) {
-            $maior_numero = $num2;
+            if ($num2 > $maior_numero) {
+                $maior_numero = $num2;
+            }
+
+            if ($num3 > $maior_numero) {
+                $maior_numero = $num3;
+            }
+
+            echo "<p class='result'>O maior número é $maior_numero</p>";
         }
-
-        if ($num3 > $maior_numero) {
-            $maior_numero = $num3;
-        }
-
-        echo "<p>O maior número é $maior_numero</p>";
-    }
-    ?>
+        ?>
+    </div>
 </body>
 </html>
